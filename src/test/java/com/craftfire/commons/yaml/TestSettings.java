6830d7937201b441046abdfa9be1920b4fcb7de8
Wolf480pl <wolf480@interia.pl>
2013-02-22 10:59:47 +0100

    Added TestSettings

---
+++ src/test/java/com/craftfire/commons/yaml/TestSettings.java
@@ -0,0 +1,117 @@
+package com.craftfire.commons.yaml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import java.util.logging.Logger;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.DumperOptions.FlowStyle;
+import org.yaml.snakeyaml.DumperOptions.LineBreak;
+import org.yaml.snakeyaml.DumperOptions.ScalarStyle;
+import org.yaml.snakeyaml.constructor.BaseConstructor;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.representer.Representer;
+import org.yaml.snakeyaml.resolver.Resolver;
+
+import com.craftfire.commons.util.LoggingManager;
+
+public class TestSettings {
+    private Settings settings;
+
+    @Before
+    public void setup() {
+        this.settings = new Settings();
+    }
+
+    @Test
+    public void testLogger() {
+        LoggingManager logger = this.settings.getLogger();
+        assertEquals("[YamlManager]", logger.getPrefix());
+        assertSame(Logger.getLogger("CraftFire.YamlManager"), logger.getLogger());
+
+        logger = mock(LoggingManager.class);
+        assertSame(this.settings, this.settings.setLogger(logger));
+        assertSame(logger, this.settings.getLogger());
+    }
+
+    @Test
+    public void testResolver() {
+        assertEquals(Resolver.class, this.settings.getResolver().getClass());
+
+        Resolver mock = mock(Resolver.class);
+        assertSame(this.settings, this.settings.setResolver(mock));
+        assertSame(mock, this.settings.getResolver());
+    }
+
+    @Test
+    public void testDumperOptions() {
+        DumperOptions options = this.settings.getDumperOptions();
+        assertEquals(FlowStyle.BLOCK, options.getDefaultFlowStyle());
+        assertEquals(ScalarStyle.PLAIN, options.getDefaultScalarStyle());
+        assertEquals(4, options.getIndent());
+        assertEquals(LineBreak.UNIX, options.getLineBreak());
+        assertNull(options.getTags());
+        assertNull(options.getTimeZone());
+        assertNull(options.getVersion());
+        assertEquals(80, options.getWidth());
+        assertFalse(options.isAllowReadOnlyProperties());
+        assertTrue(options.isAllowUnicode());
+        assertFalse(options.isCanonical());
+        assertFalse(options.isExplicitEnd());
+        assertFalse(options.isExplicitStart());
+        assertFalse(options.isPrettyFlow());
+
+        options = mock(DumperOptions.class);
+        assertSame(this.settings, this.settings.setDumperOptions(options));
+        assertSame(options, this.settings.getDumperOptions());
+    }
+
+    @Test
+    public void testRepresenter() {
+        assertEquals(EmptyNullRepresenter.class, this.settings.getRepresenter().getClass());
+
+        Representer mock = mock(Representer.class);
+        assertSame(this.settings, this.settings.setRepresenter(mock));
+        assertSame(mock, this.settings.getRepresenter());
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(Constructor.class, this.settings.getConstructor().getClass());
+
+        BaseConstructor mock = mock(BaseConstructor.class);
+        assertSame(this.settings, this.settings.setConstructor(mock));
+        assertSame(mock, this.settings.getConstructor());
+    }
+
+    @Test
+    public void testSeparator() {
+        assertEquals(".", this.settings.getSeparator());
+
+        assertSame(this.settings, this.settings.setSeparator("::"));
+        assertEquals("::", this.settings.getSeparator());
+    }
+
+    @Test
+    public void testMultiDocument() {
+        assertFalse(this.settings.isMultiDocument());
+
+        assertSame(this.settings, this.settings.setMultiDocument(true));
+        assertTrue(this.settings.isMultiDocument());
+    }
+
+    @Test
+    public void testCaseSensitive() {
+        assertFalse(this.settings.isCaseSensitive());
+
+        assertSame(this.settings, this.settings.setCaseSensitive(true));
+        assertTrue(this.settings.isCaseSensitive());
+    }
+}
